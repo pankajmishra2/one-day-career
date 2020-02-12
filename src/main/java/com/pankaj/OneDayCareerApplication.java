8800d09c7be856276aa92a5b38bbcd1ec3f253f7
pankajmishra2 <pankaj.mishra@pearson.com>
2020-02-12 06:08:32 +0530

    first commit

---
+++ src/main/java/com/pankaj/OneDayCareerApplication.java
@@ -0,0 +1,13 @@
+package com.pankaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayCareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayCareerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pankaj/OneDayCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayCareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
